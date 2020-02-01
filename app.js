ccdbacebf930145eecd680efa9d8bf8015a36e05
sam <samuel.choi@commercelab.com.au>
2020-02-01 16:58:20 +1100

    Added login and password in app.js + added some other html pages

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+
+var objPeople = [
+    {
+        username: "sam",
+        password: "apple"
+    },
+    {
+        username: "david",
+        password: "pear"
+    },
+    {
+        username: "chris",
+        password: "blueberry"
+    }
+]
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -12,3 +28,18 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for(i=0; i < objPeople.length; i++) {
+        if(username == objPeople[i].username && password == objPeople[i].password) {
+            console.log(username + " is logged in!")
+            return
+        }        
+    }
+    console.log("incorrect username or password!")
+}
+
+
